b5b5913a69c22ec229bafc9c6449b3224d7df553
João Pedro Dante <joaopdante@tryhosting.com.br>
2022-08-10 21:13:17 -0300

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# LunarBlog-Flask
---
+++ app.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from flask import Flask
+app = Flask("projeto")
+
+
+@app.route("/")
+def ola_mundo():
+    return "Olá Mundo! Esse é meu primeiro projeto Flask!!", 200
+
+
+app.run()
